e6c19f776d3d97a3627d4305ec95469ed717f257
Daniel Simols <in.lieu.of.leu@gmail.com>
2024-02-03 22:09:54 +0000

    Add a 'finished' page

---
+++ src/pages/student/finished.tsx
@@ -0,0 +1,10 @@
+export default function Finished() {
+    const score_percent = "54%"
+    return (
+      <main className={`flex min-h-screen flex-col items-center p-24`}>
+        <div className="text-xl w-screen text-center font-bold">
+            You've finished the quiz! You got {score_percent}!
+        </div>
+      </main>
+  )
+}
