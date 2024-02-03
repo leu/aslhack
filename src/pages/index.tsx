c3bb79fdb7c77bf1a270f0c569c2bbb0ac0a7b54
Daniel Simols <in.lieu.of.leu@gmail.com>
2024-02-03 19:37:28 +0000

    Integrate a 'Join Quiz' with text input field on root page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
+import { TextField } from "@mui/material";
+
 export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center p-24`}>
       <a href="/teacher/setup">
         <div className={`text-xl font-bold bg-orange-600 text-white border-orange-950 border-0 px-6 py-4 rounded-lg w-60 mb-10`}>
-          Create Classroom
+          Create Quiz
         </div>
       </a>
+      <div className="mb-6">OR</div>
+      <input type="text" className="mb-4 text-black w-60 h-8 px-2 mt-2" placeholder="Classroom ID"/>
       <a href="#">
         <div className={`text-xl font-bold bg-orange-600 text-white border-orange-950 border-0 px-6 py-4 rounded-lg w-60`}>
-          Join Classroom
+          Take Quiz
         </div>
       </a>
     </main>
--- src/pages/student/join.tsx
+++
@@ -1,6 +0,0 @@
-export default function Join() {
-    return (
-      <main className={`flex min-h-screen flex-col items-center p-24`}>
-      </main>
-  );
-}
